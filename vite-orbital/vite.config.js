45a7008a1aade9746f5badb61bb7022f975b2d95
irw9n <irwanahmed001@gmail.com>
2025-06-30 23:30:04 +0800

    viteconfig.js

--- vite-orbital/vite.config.js
+++ vite-orbital/vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
 
-// https://vite.dev/config/
+const certPath = './localhost+1.pem'; 
+const keyPath = './localhost+1-key.pem'; 
+
+const hasCerts = fs.existsSync(certPath) && fs.existsSync(keyPath);
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, 
+    port: 5173,
+    ...(hasCerts ? {
+      https: {
+        key: fs.readFileSync(keyPath),
+        cert: fs.readFileSync(certPath),
+      },
+    } : {}), 
+  }
 })
